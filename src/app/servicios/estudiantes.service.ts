74b2ece0c81d87ca6948917dc1e64b990017cf79
Jhonatan Oro Anticona <142758666+JhowillSC@users.noreply.github.com>
2025-08-08 15:49:50 -0500

    Añadiendo nueva columna a la vista (SEDE)

--- src/app/servicios/estudiantes.service.ts
+++ src/app/servicios/estudiantes.service.ts
@@ -9,18 +9,19 @@ export interface Estudiante {
   apellidoMaterno: string;
   estadoAdmision: string | null;
   numDoc: string;
+  sede:string;
 }
 
 @Injectable({
   providedIn: 'root'
 })
 export class EstudiantesService {
-  // CAMBIO 1: Solo la URL base sin /alumno
+  
   private urlBase = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
-  // CAMBIO 2: Construir la URL correctamente con el número como parámetro de ruta
+  
   public getEstudiantePorNumDoc(numDoc: string): Observable<Estudiante> {
     const url = `${this.urlBase}/alumno/${numDoc}`;
     console.log('URL que se está llamando:', url);
